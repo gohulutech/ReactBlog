e0944b9c9887abe788bc6cb0c40d5894369903a4
Hugo Arevalo <hugoarevalo@Hugos-MacBook-Pro.local>
2021-06-24 22:44:18 -0500

    Finished project

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchPostsAndUsers } from "../actions";
 
-const PostList = () => {
-  return <div>Post List</div>;
+import UserHeader from "./UserHeader";
+
+const PostList = ({ fetchPostsAndUsers, posts }) => {
+  useEffect(() => {
+    fetchPostsAndUsers();
+  }, [fetchPostsAndUsers]);
+
+  const renderList = () => {
+    return posts.map((post) => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+            <UserHeader userId={post.userId} />
+          </div>
+        </div>
+      );
+    });
+  };
+  return <div className="ui relaxed divided list">{renderList()}</div>;
 };
 
-export default PostList;
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList);
---
+++ src/components/UserHeader.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const UserHeader = ({ user }) => {
+  const renderUser = () => {
+    if (!user) {
+      return null;
+    }
+
+    return <div className="header">{user.name}</div>;
+  };
+
+  return <div className="header">{renderUser()}</div>;
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find((user) => user.id === ownProps.userId) };
+};
+
+export default connect(mapStateToProps, {})(UserHeader);
